aecec07bc7ccf1664c57a99c7b572e44171fa06a
tomi <tomasarmano17@gmail.com>
2024-11-01 15:33:13 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { ToastContainer } from "react-toastify"
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainerWithHoc from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import Form from './components/Form/Form'
 import Cart from './components/Cart/Cart'
 import Checkout from "./components/Checkout/Checkout"
 import 'react-toastify/dist/ReactToastify.css';
@@ -22,7 +21,6 @@ function App() {
             <Route path='/' element={ <ItemListContainerWithHoc/> } />
             <Route path='/category/:idCategory' element={ <ItemListContainerWithHoc/> } />
             <Route path='/detail/:idProduct' element={ <ItemDetailContainer/> } />
-            <Route path='/micuenta' element={ <Form/> } />
             <Route path='/cart' element={ <Cart/> } />
             <Route path="/checkout" element={<Checkout />} />
           </Routes>
